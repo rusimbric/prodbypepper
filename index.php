95c099be4cfbc1d0bf9e2dafa23c45a0dfb14135
rusimbric <131174257+rusimbric@users.noreply.github.com>
2024-04-20 19:49:19 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    
+<nav>
+<div class="logo"><a href="#home"><i class="fa-solid fa-pepper-hot"></i><div class="cont">Pepper</div></a></div>
+<div class="links">
+<a href="#home"><i class="fa-solid fa-house"></i><div class="cont">Home </div></a>
+<a href="#about"> <i class="fa-solid fa-address-card"></i><div class="cont">About</div></a>
+<a href="#contact"><i class="fa-solid fa-address-book"></i><div class="cont">Contact </div></a>
+
+</div>
+</nav>
+<section id="home">
+<div class="img">
+<!-- <img src="3.jpg" alt=""> -->
+<div class="right">
+<div class="text">
+Hey, I'm <span>Pepper</span>, a music producer proudly representing Rwanda. 
+Over the past three years, I've been diving deep into the world of music production, crafting beats and melodies that reflect the vibrant spirit of my homeland.
+<div class="audio-player">
+  <div class="audio-controls">
+    <audio controls autoplay>
+      <source src="local drill.wav" type="audio/mpeg">
+      Your browser does not support the audio element.
+    </audio>
+  </div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+
+
+<section id="about">
+    <div class="all">
+<br><br><br>        
+        <br><br><br>
+    <div class="left">
+    <div class="text">
+    <h2><u>About Me:</u></h2>
+        <br><br>
+        <p>I'm a music producer based in Rwanda. Infused with the rich cultural fabric of Rwanda, my music is a vibrant fusion of traditional African beats and contemporary influences. From the hypnotic groove of the djembe to the soulful melodies of the kora, every track I produce is a celebration of the rhythmic diversity of the African continent.
+
+As an Afrobeat artist, I'm passionate about using music as a force for unity and empowerment. Through my beats and lyrics, I strive to inspire positivity, ignite passion, and celebrate the resilience of the human spirit.</div></div>
+
+<div class="right">
+    <div class="text">
+    <h2><u>Skills:</u></h2>
+        <br><br>
+        <ul>
+            <li><i class="fa-solid fa-music"></i>Music Production</li>
+            <li><i class="fa-solid fa-headphones"></i>Mixing & Mastering</li>
+            <li><i class="fa-solid fa-microphone"></i>Vocal Recording</li>
+            <li><i class="fa-solid fa-globe"></i>Web Development</li>
+        </ul>
+    </div>
+</div></div>
+</div>
+</section>
+
+
+
+<section id="contact">
+    <!-- <br><br><br> -->
+    <div class="all">
+        <div class="left">
+    <h2><u>Contact:</u></h2>
+    <br><br>
+    <div class="tel">
+        <i class="fa-solid fa-address-book"></i>:+250 790 356 694
+    </div>
+<div class="address">
+<i class="fa-solid fa-location-dot"></i>:Rwanda, Kigali, Gikondo
+</div>
+<div class="email">
+<i class="fa-solid fa-envelope"></i>:manzirusimbi.com@gmail.com
+</div>
+
+<div class="instagram">
+<i class="fa-brands fa-square-instagram"></i>:
+<a href="https://www.instagram.com/prodby_pepper/" target="_blank">@prodby_pepper</a>
+</div>
+<div class="linkee">
+<i class="fa-solid fa-link"></i>:
+    <a href="https://linktr.ee/prod_pepper" target="_blank">prod_pepper</a>
+</div>
+<div class="youtube">
+<i class="fa-brands fa-youtube"></i>:
+    <a href="https://www.youtube.com/channel/UCAm2IVgHCb8hYyWKgbJDD4A" target="_blank">Pepper</a>
+</div>
+</div>
+
+    <center>
+        <hr>
+    </center>
+
+    
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d11278.256174463717!2d30.080888038845366!3d-1.967693138390328!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x19dca61434d56e91%3A0x9f4e8ed1622d9f06!2sGikondo%2C%20Kigali!5e0!3m2!1sen!2srw!4v1713446329150!5m2!1sen!2srw" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </div>
+</section>
+</body>
+</html>
